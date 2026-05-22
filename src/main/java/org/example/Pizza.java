f1ff7438582e30b61510f8b14670ee2bcf927c57
yathisha-coder <yathisai85@gmail.com>
2026-05-22 13:03:28 -0400

    In pizza class implemented the list for topping and added the methods.

--- src/main/java/org/example/Pizza.java
+++ src/main/java/org/example/Pizza.java
@@ -1,17 +1,38 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pizza extends MenuItem {
     private PizzaSize size;
     private CrustType crust;
     private boolean stuffedCrust;
 
+    //Lists to store pizza ingredients
+    private List<String> meats = new ArrayList<>();
+    private List<String> cheeses = new ArrayList<>();
+    private List<String> regularTopping = new ArrayList<>();
+    private List<String> sauces = new ArrayList<>();
+    //Constructor
     public Pizza(String name,PizzaSize size,CrustType crust,boolean stuffedCrust) {
         super(name);
         this.size = size;
         this.crust = crust;
         this.stuffedCrust = stuffedCrust;
     }
-
+    //Add Toppings methods
+    public void addMeat(String meat){
+        meats.add(meat);
+    }
+    public void addCheese(String cheese){
+        cheeses.add(cheese);
+    }
+    public void addRegularTopping(String topping){
+        regularTopping.add(topping);
+    }
+    public void addSauce(String sauce){
+        sauces.add(sauce);
+    }
     @Override
     public double calculatePrice() {
         double price = 0;
