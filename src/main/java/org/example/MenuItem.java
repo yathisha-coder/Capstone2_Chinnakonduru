452e09d9300f6d02fdcf2b8818ba1235f0d0ed7b
yathisha-coder <yathisai85@gmail.com>
2026-05-22 10:28:28 -0400

    In Menu Item class created the constructor and getter. In Pizza class implemented the properties and parents class constructor and methods.

--- src/main/java/org/example/MenuItem.java
+++ src/main/java/org/example/MenuItem.java
@@ -2,6 +2,14 @@ package org.example;
 
 public abstract class MenuItem {
     protected String name;
+    //Constructor
+    public MenuItem(String name){
+        this.name = name;
+    }
+    //Getter
+    public  String  getName(){
+        return name;
+    }
     //Method for calculate the price
     public abstract double calculatePrice();
     //Method for receipt
--- src/main/java/org/example/Pizza.java
+++ src/main/java/org/example/Pizza.java
@@ -1,4 +1,21 @@
 package org.example;
 
-public class Pizza {
+public class Pizza extends MenuItem {
+    private PizzaSize size;
+    private CrustType crust;
+    private boolean stuffedCrust;
+
+    public Pizza(String name) {
+        super(name);
+    }
+
+    @Override
+    public double calculatePrice() {
+        return 0;
+    }
+
+    @Override
+    public String getReceiptText() {
+        return "";
+    }
 }
