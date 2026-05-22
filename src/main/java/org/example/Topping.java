2975239a89873241490932a710d6c43fe63441f2
yathisha-coder <yathisai85@gmail.com>
2026-05-22 16:18:31 -0400

    Implemented the Topping class method and logic.

--- src/main/java/org/example/Topping.java
+++ src/main/java/org/example/Topping.java
@@ -1,25 +1,64 @@
 package org.example;
 
-public class Topping extends MenuItem {
+import static org.example.CrustType.REGULAR;
+import static org.example.ToppingType.SAUCE;
+
+public class Topping {
+    private String name;
     //Meat, Cheese, regular, sauce
     private ToppingType type;
+    private boolean extra;
     //Constructor
-    public Topping(String name) {
-        super(name);
+    public Topping(String name,ToppingType type, boolean extra) {
+            this.name = name;
+            this.type = type;
+            this.extra = extra;
     }
-
-    @Override
-    public double calculatePrice() {
-        return 0;
+    //Getters
+    public String getName(){
+        return name;
     }
-
+    public ToppingType getType(){
+        return type;
+    }
+    public boolean isExtra(){
+        return extra;
+    }
+    //Topping price depends on pizza size
     public double calculatePrice(PizzaSize size) {
         double price = 0;
+        switch (type){
+            case MEAT -> {
+                switch (size) {
+                    case PERSONAL -> price = 1.00;
+                    case MEDIUM -> price = 2.00;
+                    case LARGE -> price = 3.00;
+                }
+                if (extra) {
+                    switch (size) {
+                        case PERSONAL -> price += 0.30;
+                        case MEDIUM -> price += 0.60;
+                        case LARGE -> price += 0.90;
+                    }
+                }
+            }
+            case CHEESE -> {
+                switch (size){
+                    case PERSONAL -> price = 0.75;
+                    case LARGE -> price = 1.50;
+                    case MEDIUM -> price = 2.25;
+                }
+                if(extra){
+                    switch (size){
+                        case PERSONAL -> price += 0.30;
+                        case MEDIUM -> price += 0.60;
+                        case LARGE -> price += 0.90;
+                    }
+                }
+            }
+            case REGULAR -> price = 0.0;
+            case SAUCE -> price = 0.0;
+        }
         return price;
-    }
-
-    @Override
-    public String getReceiptText() {
-        return "";
     }
 }
--- src/main/java/org/example/ToppingType.java
+++ src/main/java/org/example/ToppingType.java
@@ -3,5 +3,6 @@ package org.example;
 public enum ToppingType {
     MEAT,
     CHEESE,
-    REGULAR
+    REGULAR,
+    SAUCE
 }
