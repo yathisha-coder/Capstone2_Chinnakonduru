2af02ce51fd17f280112f6d04a2da5dca5196f6c
yathisha-coder <yathisai85@gmail.com>
2026-05-22 11:01:37 -0400

    In Pizza class implemented the methods and logic.

--- src/main/java/org/example/Pizza.java
+++ src/main/java/org/example/Pizza.java
@@ -5,17 +5,35 @@ public class Pizza extends MenuItem {
     private CrustType crust;
     private boolean stuffedCrust;
 
-    public Pizza(String name) {
+    public Pizza(String name,PizzaSize size,CrustType crust,boolean stuffedCrust) {
         super(name);
+        this.size = size;
+        this.crust = crust;
+        this.stuffedCrust = stuffedCrust;
     }
 
     @Override
     public double calculatePrice() {
-        return 0;
+        double price = 0;
+        switch (size){
+            case PERSONAL -> price = 8.50;
+            case MEDIUM ->  price = 12.00;
+            case LARGE -> price = 16.50;
+        }
+        //Ternary Operator
+        String crustText = stuffedCrust ? "Yes" : "No";
+        return price;
     }
 
     @Override
     public String getReceiptText() {
-        return "";
+        return "======PIZZA======" +
+                "Name: " + name + "\\n" +
+                "Size: " + size + "\\n" +
+                "Crust: " + crust + "\\n" +
+                "Stuffed Crust: " +
+                (stuffedCrust ? "Yes" : "No") + "\\n" +
+                "Price: $" +
+                String.format("%.2f", calculatePrice()) + "\\n";
     }
 }
