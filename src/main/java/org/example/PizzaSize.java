b9a09bbaf142bd137a301a77e37986adcaef6c9a
yathisha-coder <yathisai85@gmail.com>
2026-05-22 13:07:36 -0400

    Implemented the PizzaSize class added the constructor and method for base price.

--- src/main/java/org/example/PizzaSize.java
+++ src/main/java/org/example/PizzaSize.java
@@ -1,8 +1,20 @@
 package org.example;
 
 public enum PizzaSize {
-    PERSONAL,
-    MEDIUM,
-    LARGE
+    PERSONAL(8.50),
+    MEDIUM(12.00),
+    LARGE(16.50);
+    //Base Pizza Price
+    private double basePrice;
+
+    //Constructor
+    PizzaSize(double basePrice){
+        this.basePrice = basePrice;
+    }
+
+    //Getter
+    public double getBasePrice(){
+        return  basePrice;
+    }
 
 }
